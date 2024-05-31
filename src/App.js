742fd1bc3156b1107e9bceb951ecccd7fba5d446
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2024-05-31 09:44:57 -0300

    fix: Recuperação das variáveis de resposta.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function FrmEquacaoSegundoGrau() {
     const [valorA, setValorA] = useState(0);    
     const [valorB, setValorB] = useState(0);
     const [valorC, setValorC] = useState(0);
-    const [resultado, setResultado] = useState('');
+    const [resposta, setResposta] = useState({a: 0, b: 0, c: 0, delta:0, x1: 0, x2:0});
   
     // Submissão do formulário.
     const handleSubmit = (event) => {
       // Impede o recarregamento da página
       event.preventDefault();      
-      //Endereço da API + operação + valorA + valorB
+      //Endereço da API + "/raizes" + "/" + valorA + "/" + valorB + "/" + valorC
       fetch(`http://localhost:8000/raizes/${valorA}/${valorB}/${valorC}`)
         .then((response) => response.json()) //Converte a resposta para JSON
-        .then((data) => setResultado(data)) // Atribui o resultado a x1
+        .then((data) => setResposta({a: data.a, b : data.b, c: data.c, delta: data.delta, x1: data.x1, x2: data.x2})); //Atribui a resposta as variáveis
     }
 
     // Limpa os campos do formulário.     
@@ -24,7 +24,7 @@ function FrmEquacaoSegundoGrau() {
       setValorA(0);
       setValorB(0);      
       setValorC(0);
-      setResultado('');
+      setResposta({a: 0, b: 0, c: 0, delta:0, x1: 0, x2:0});
     }
   
     // Renderiza o formulário
@@ -44,9 +44,9 @@ function FrmEquacaoSegundoGrau() {
         <input type="submit" name="Calcular" value="Calcular"/><br/><br/>
 
         <label>Resultado: </label><br/>
-        <label>delta: {resultado.delta} </label><br/>
-        <label>x1: {resultado.x1} </label><br/>
-        <label>x2: {resultado.x2} </label><br/>
+        <label>delta: {resposta.delta} </label><br/>
+        <label>x1: {resposta.x1} </label><br/>
+        <label>x2: {resposta.x2} </label><br/>
     </form>
     )
   }
